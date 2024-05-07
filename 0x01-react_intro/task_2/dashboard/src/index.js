b5c0fb4e4bbe83d4ac304cb25b1a1741a41ad78f
Joan254 <joanotieno1998@gmail.com>
2024-05-07 18:13:02 +0300

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -7,14 +7,8 @@ import Notifications from "./Notifications";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Notifications/>
     <App />
-  </React.StrictMode>
-);
-
-const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
-rootNotifications.render(
-  <React.StrictMode>
-    <Notifications />
   </React.StrictMode>
 );
 
