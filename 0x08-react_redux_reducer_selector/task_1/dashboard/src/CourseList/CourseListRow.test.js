8bf6aa23c3116e2683750df8d5363003a4bbb706
Joan254 <joanotieno1998@gmail.com>
2024-06-10 22:30:37 +0300

    Use Immutable for the UI Reducer

---
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Course List Row component test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should render one cell with colspan = 2 when textSecondCell is null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
+
+    expect(wrapper.find('tr').children()).toHaveLength(1);
+    expect(wrapper.find('tr').childAt(0).html()).toContain('<th colSpan="2"');
+    expect(wrapper.find('th').text()).toEqual('test');
+  });
+
+  it('should render two cells when textSecondCell is not null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />);
+
+    expect(wrapper.find('tr').children()).toHaveLength(2);
+    expect(wrapper.find('tr').childAt(0).html()).toContain('<td');
+    expect(wrapper.find('td').at(0).text()).toContain('test');
+    expect(wrapper.find('td').at(1).text()).toContain('test');
+  });
+});
