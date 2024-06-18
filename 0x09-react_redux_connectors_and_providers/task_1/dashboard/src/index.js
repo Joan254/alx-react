0fe3be44184688dbeaabfe5ce256b63ab1b5844f
Joan254 <joanotieno1998@gmail.com>
2024-06-18 16:24:43 +0300

    Update mapStateToProps

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App/App';
+import uiReducer from './reducers/uiReducer';
+
+const store = createStore(uiReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
